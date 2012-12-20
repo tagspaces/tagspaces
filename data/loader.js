e67c8bff32f063de9f1c4f5dcdc4f4f38a5fc79a
uggrock <ugg.rock@gmail.com>
2012-12-20 13:36:56 +0100

    jquery-ui-custom library exchanged with the the developers version of it

--- data/loader.js
+++ data/loader.js
@@ -11,12 +11,21 @@ require.config({
     },
     paths: {
         jquery: 'libs/jquery/jquery-1.8.3',
+        dynatree: 'libs/dynatree/jquery.dynatree.min',
+        dynatreecss: 'libs/dynatree/skin-vista/ui.dynatree',   
         jquerylayout: 'libs/jquerylayout/jquery.layout-latest.min',
         jquerylayoutcss: 'libs/jquerylayout/layout-default-latest',
-        jqueryui: 'libs/jqueryui/jquery-ui-1.9.2.custom',
+        jqueryui: 'libs/jqueryui/jquery.ui.core',
+        jqueryuiwidget: 'libs/jqueryui/jquery.ui.widget',
+        jqueryuimouse: 'libs/jqueryui/jquery.ui.mouse', 
+        jqueryuidraggable: 'libs/jqueryui/jquery.ui.draggable',
+        jqueryuiposition: 'libs/jqueryui/jquery.ui.position',
+        jqueryuiresizable: 'libs/jqueryui/jquery.ui.resizable',
+        jqueryuimenu: 'libs/jqueryui/jquery.ui.menu',
+        jqueryuibutton: 'libs/jqueryui/jquery.ui.button', 
+        jqueryuidialog: 'libs/jqueryui/jquery.ui.dialog',              
+        jqueryuiselectable: 'libs/jqueryui/jquery.ui.selectable',        
         jqueryuicss: 'libs/jqueryui/custom-theme/jquery-ui-1.9.2.custom',
-        dynatree: 'libs/dynatree/jquery.dynatree.min',
-        dynatreecss: 'libs/dynatree/skin-vista/ui.dynatree',        
         datatables: 'libs/datatables/js/jquery.dataTables.min',
         datatablescss: 'libs/datatables/css/jquery.dataTables',
         jsoneditor: 'libs/jsoneditor/jsoneditor',
@@ -31,7 +40,16 @@ require.config({
         },
         'jqueryui': {
             deps: [
-                'jquery'
+                'jquery',
+                'jqueryuiwidget',
+                'jqueryuimouse', 
+                'jqueryuidraggable',
+                'jqueryuiposition',
+                'jqueryuiresizable',
+                'jqueryuimenu',
+                'jqueryuibutton', 
+                'jqueryuidialog',              
+                'jqueryuiselectable',    
             ]
         },
         'dynatree': {
