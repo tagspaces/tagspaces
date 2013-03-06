53cf37331bce77bf0a28b2f0cda6b076e205c66b
uggrock <ugg.rock@gmail.com>
2013-03-06 16:13:27 +0100

    Copyright information updated

--- data/ext/editorHTML/extension.js
+++ data/ext/editorHTML/extension.js
@@ -1,3 +1,7 @@
+/* Copyright (c) 2013 The Tagspaces Authors. All rights reserved.
+ * Use of this source code is governed by a AGPL3 license that 
+ * can be found in the LICENSE file. */
+
 define(function(require, exports, module) {
 "use strict";
 
--- data/ext/viewRiver/extension.js
+++ data/ext/viewRiver/extension.js
@@ -1,4 +1,4 @@
-/* Copyright (c) 2012 The Tagspaces Authors. All rights reserved.
+/* Copyright (c) 2013 The Tagspaces Authors. All rights reserved.
  * Use of this source code is governed by a AGPL3 license that 
  * can be found in the LICENSE file. */
 
