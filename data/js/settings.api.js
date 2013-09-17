6c347642c3043ed891484bea1a92b9a2088823f3
uggrock <ugg.rock@gmail.com>
2013-09-17 15:12:11 +0200

    Imporoving the firefox's toolbar button integration

--- data/js/settings.api.js
+++ data/js/settings.api.js
@@ -1,4 +1,4 @@
-/* Copyright (c) 2012 The Tagspaces Authors. All rights reserved.
+/* Copyright (c) 2012-2013 The TagSpaces Authors. All rights reserved.
  * Use of this source code is governed by a AGPL3 license that 
  * can be found in the LICENSE file. */
 define(function(require, exports, module) {
