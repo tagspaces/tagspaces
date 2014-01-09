c00123ceb28973ce14aced0346ad9906d07756bd
Ilian <mail@ilian.me>
2014-01-09 23:17:03 +0100

    adding directory browser functionality refactoring dirSeparator

--- data/js/directories.ui.js
+++ data/js/directories.ui.js
@@ -188,7 +188,7 @@ define(function(require, exports, module) {
                                 var fileName = TSCORE.TagUtils.extractFileName(filePath);
                                 var targetDir = $(this).attr("key");
                                 console.log("Moving file: "+filePath+" to "+targetDir);
-                                TSCORE.IO.renameFile(filePath, targetDir+TSCORE.TagUtils.DIR_SEPARATOR+fileName);
+                                TSCORE.IO.renameFile(filePath, targetDir+TSCORE.dirSeparator+fileName);
                                 $(ui.helper).remove();                                 
                         }                  
                     }
@@ -246,7 +246,7 @@ define(function(require, exports, module) {
                                     var fileName = TSCORE.TagUtils.extractFileName(filePath);
                                     var targetDir = $(this).attr("key");
                                     console.log("Moving file: "+filePath+" to "+targetDir);
-                                    TSCORE.IO.renameFile(filePath, targetDir+TSCORE.TagUtils.DIR_SEPARATOR+fileName);
+                                    TSCORE.IO.renameFile(filePath, targetDir+TSCORE.dirSeparator+fileName);
                                     $(ui.helper).remove();  
                                 }                              
                             }                   
@@ -327,7 +327,7 @@ define(function(require, exports, module) {
         
         // If directory path not in history then add it to the history
         if(directoryFoundOn < 0) {      
-            var parentLocation = directoryPath.substring(0, directoryPath.lastIndexOf(TSCORE.TagUtils.DIR_SEPARATOR));
+            var parentLocation = directoryPath.substring(0, directoryPath.lastIndexOf(TSCORE.dirSeparator));
             var parentFound = -1;
             for(var i=0; i < directoryHistory.length; i++) {
                 if(directoryHistory[i].path == parentLocation) {
@@ -341,7 +341,7 @@ define(function(require, exports, module) {
                 }    
             }  
                     
-            var locationTitle = directoryPath.substring(directoryPath.lastIndexOf(TSCORE.TagUtils.DIR_SEPARATOR)+1,directoryPath.length);
+            var locationTitle = directoryPath.substring(directoryPath.lastIndexOf(TSCORE.dirSeparator)+1,directoryPath.length);
             directoryHistory.push({
                 "name": locationTitle,
                 "path" : directoryPath,
@@ -407,6 +407,24 @@ define(function(require, exports, module) {
         initLocations();  
         openLocation($("#folderLocation2").val());                                         
     }
+    
+    function selectLocalDirectory() {
+        if(isChrome) { // isCordova
+            require([
+                  "text!templates/DirectoryBrowserDialog.html",
+                  "tsdirectorybrowser"
+                ], function(uiTPL, controller) {
+                    if($("#directoryBrowserDialog").length < 1) {                
+                        var uiTemplate = Handlebars.compile( uiTPL );
+                        $('body').append(uiTemplate());   
+                        controller.initUI();                        
+                    }
+                    controller.reInitUI();                    
+            });
+        } else {
+            TSCORE.IO.selectDirectory();                    
+        }
+    }
 
     function showLocationEditDialog(name,path) {
         require([
@@ -419,7 +437,7 @@ define(function(require, exports, module) {
                                     
                     $("#selectLocalDirectory2").on("click",function(e) {
                         e.preventDefault();
-                        TSCORE.IO.selectDirectory();
+                        selectLocalDirectory();
                     }); 
                     
                     
@@ -461,7 +479,7 @@ define(function(require, exports, module) {
 			     		            
 			        $("#selectLocalDirectory").on("click",function(e) {
 			            e.preventDefault();
-			            TSCORE.IO.selectDirectory();
+                        selectLocalDirectory();
 			        }); 	
 			        
                     TSCORE.Config.getActivatedPerspectiveExtensions().forEach( function(value) {
@@ -500,7 +518,7 @@ define(function(require, exports, module) {
 			            // bValid = bValid && checkRegexp( email, /^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i, "eg. ui@jquery.com" );
 			            // bValid = bValid && checkRegexp( password, /^([0-9a-zA-Z])+$/, "Password field only allow : a-z 0-9" );
 			            if ( bValid ) {
-			                TSCORE.IO.createDirectory(dir4ContextMenu+TSCORE.TagUtils.DIR_SEPARATOR+$( "#newDirectoryName" ).val());
+			                TSCORE.IO.createDirectory(dir4ContextMenu+TSCORE.dirSeparator+$( "#newDirectoryName" ).val());
 			                //navigateToDirectory(dir4ContextMenu);
 			            }
 			        });   			     	            
