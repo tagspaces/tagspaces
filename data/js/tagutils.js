567fbb9aa3fae3c94e2a4ebdcc222a84fe5aaad2
Ilian <ilian@tagspaces.org>
2014-04-24 23:24:04 +0200

    jshinting

--- data/js/tagutils.js
+++ data/js/tagutils.js
@@ -1,7 +1,8 @@
 /* Copyright (c) 2012-2014 The Tagspaces Authors. All rights reserved.
  * Use of this source code is governed by a AGPL3 license that 
  * can be found in the LICENSE file. */
-define(function(require, exports, module) {
+/* global define  */
+ define(function(require, exports, module) {
 "use strict";
 
     console.log("Loading tagutils.js ...");
