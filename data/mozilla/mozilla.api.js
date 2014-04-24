56755bfe195288c22c1a6bef705955a945f32efd
Ilian <ilian@tagspaces.org>
2014-04-24 19:59:56 +0200

    jshinting

--- data/mozilla/mozilla.api.js
+++ data/mozilla/mozilla.api.js
@@ -14,15 +14,15 @@ define(function(require, exports, module) {
         console.log("Message received in page script from content script: "); //+JSON.stringify(event.detail));
         var message = event.detail;
         switch (message.command) {
-          case "loadSettings":
+        case "loadSettings":
             if(message.success) {
                 try {
                     console.log("Loading settings...: "+JSON.stringify(message.content));
                     TSCORE.Config.updateSettingMozillaPreferences(message.content);
-    
+
                     TSCORE.initLocations();
                     TSCORE.generateTagGroups();
-                      
+
                 } catch (ex) {
                     console.log("Exception while getting setting from firefox failed "+ex);
                 }
@@ -30,92 +30,93 @@ define(function(require, exports, module) {
                 console.log("Getting setting from firefox failed");
             }
             break;
-          case "saveSettings":
+        case "saveSettings":
             if(message.success) {
                 console.log("Saving setting as native mozilla preference successfull!");
             } else {
-                console.log("Saving setting as native mozilla preference failed!");            
+                console.log("Saving setting as native mozilla preference failed!");
             }
-            break;        
-          case "rename":
+            break;
+        case "rename":
             if(message.success){
                 TSPOSTIO.renameFile(message.content[0],message.content[1]);
             } else {
-                TSCORE.updateLogger("Rename failed");        
+                TSCORE.updateLogger("Rename failed");
             }
             break;
-          case "saveTextFile":
+        case "saveTextFile":
             if(message.success){
                 TSPOSTIO.saveTextFile(message.content);
             } else {
-                TSCORE.updateLogger("Save failed");      
+                TSCORE.updateLogger("Save failed");
             }
             break;
-          case "createDirectory":
+        case "createDirectory":
             if(message.success){
                 TSPOSTIO.createDirectory(message.content);
             } else {
-                TSCORE.updateLogger("Create dir failed");        
+                TSCORE.updateLogger("Create dir failed");
             }
             break;
-          case "loadTextFile":
+        case "loadTextFile":
             if(message.success){
                 TSPOSTIO.loadTextFile(message.content);
-            } else {                
-                TSCORE.updateLogger("File loading failed");      
+            } else {
+                TSCORE.updateLogger("File loading failed");
             }
             break;
-          case "listDirectory":
+        case "listDirectory":
             if(message.success){
-                TSPOSTIO.listDirectory(message.content);       
+                TSPOSTIO.listDirectory(message.content);
             } else {
                 TSPOSTIO.errorOpeningPath();
             }
-            break;      
-          case "indexDirectory":
+            break;
+        case "indexDirectory":
             if(message.success){
                 TSPOSTIO.createDirectoryIndex(message.content);
             } else {
-                TSCORE.updateLogger("Indexing directory failed");        
+                TSCORE.updateLogger("Indexing directory failed");
             }
-            break;  
-          case "createDirectoryTree":
+            break;
+        case "createDirectoryTree":
             if(message.success){
                 console.log("Directory tree: "+JSON.stringify(message.content));
-                TSPOSTIO.createDirectoryTree(message.content);            
+                TSPOSTIO.createDirectoryTree(message.content);
             } else {
-                TSCORE.updateLogger("Indexing directory failed");        
+                TSCORE.updateLogger("Indexing directory failed");
             }
-            break;  
-          case "delete":
+            break;
+        case "delete":
             if(message.success){
-                TSPOSTIO.deleteElement(message.content);         
+                TSPOSTIO.deleteElement(message.content);
             } else {
-                TSCORE.updateLogger("Delete failed");        
+                TSCORE.updateLogger("Delete failed");
             }
-            break;          
-          case "selectDirectory":
+            break;
+        case "selectDirectory":
             if(message.success){
                 TSPOSTIO.selectDirectory(message.content);
             } else {
-                TSCORE.updateLogger("Selecting directory failed.");        
+                TSCORE.updateLogger("Selecting directory failed.");
             }
-            break;  
-          case "checkNewVersion":
+            break;
+        case "checkNewVersion":
             if(message.success){
                 TSPOSTIO.checkNewVersion(message.content);
             } else {
-                TSCORE.updateLogger("Checking for new version failed.");        
+                TSCORE.updateLogger("Checking for new version failed.");
             }
-          case "getFileProperties":
+            break;
+        case "getFileProperties":
             if(message.success){
                 TSPOSTIO.getFileProperties(message.content);
             } else {
-                TSCORE.updateLogger("Getting file properties failed.");        
+                TSCORE.updateLogger("Getting file properties failed.");
             }
-            break;                  
-          default:
             break;
+        default:
+        break;
         }   
     }, false);
     
@@ -141,70 +142,70 @@ define(function(require, exports, module) {
     };
     
     var createDirectory = function(dirPath) {
-    	console.log("Directory "+dirPath+" created.");
+        console.log("Directory "+dirPath+" created.");
         TSCORE.showLoadingAnimation();			
         var event = document.createEvent('CustomEvent');
         event.initCustomEvent("addon-message", true, true, {"detail":{
-    		"command": "createDirectory",
-    		"path": dirPath
-    	}});
+            "command": "createDirectory",
+            "path": dirPath
+        }});
         document.documentElement.dispatchEvent(event);
     };
     
     var loadTextFile = function(filePath) {
-    	console.log("Loading file: "+filePath);
+        console.log("Loading file: "+filePath);
         TSCORE.showLoadingAnimation();	
         var event = document.createEvent('CustomEvent');
         event.initCustomEvent("addon-message", true, true, {"detail":{
-    		"command": "loadTextFile",
-    		"path": filePath
-    	}});
+            "command": "loadTextFile",
+            "path": filePath
+        }});
         document.documentElement.dispatchEvent(event);	
     };
     
     var renameFile = function(filePath, newFilePath) {
-    	console.log("Renaming "+filePath+" to "+newFilePath);
+        console.log("Renaming "+filePath+" to "+newFilePath);
         TSCORE.showLoadingAnimation();
         var event = document.createEvent('CustomEvent');
         event.initCustomEvent("addon-message", true, true, {"detail":{
-    		"command": "rename",
-    		"path": filePath,
-    		"newPath": newFilePath	
-    	}});
+            "command": "rename",
+            "path": filePath,
+            "newPath": newFilePath
+        }});
         document.documentElement.dispatchEvent(event);
     };
     
     var saveTextFile = function(filePath,content) {
-    	console.log("Saving file: "+filePath);
+        console.log("Saving file: "+filePath);
         TSCORE.showLoadingAnimation();	
         var event = document.createEvent('CustomEvent');
         event.initCustomEvent("addon-message", true, true, {"detail":{
-    		"command": "saveTextFile",
-    		"path": filePath,
-    		"content": content	
-    	}});
+            "command": "saveTextFile",
+            "path": filePath,
+            "content": content
+        }});
         document.documentElement.dispatchEvent(event);	
     };
     
     var listDirectory = function(dirPath) {
-    	console.log("Listing directory: "+dirPath);
+        console.log("Listing directory: "+dirPath);
         TSCORE.showLoadingAnimation();	
         var event = document.createEvent('CustomEvent');
         event.initCustomEvent("addon-message", true, true, {"detail":{
-    		"command": "listDirectory",
-    		"path": dirPath
-    	}});
+            "command": "listDirectory",
+            "path": dirPath
+        }});
         document.documentElement.dispatchEvent(event);		
     };
     
     var deleteElement = function(path) {
-    	console.log("Deleting: "+path);
+        console.log("Deleting: "+path);
         TSCORE.showLoadingAnimation();	
         var event = document.createEvent('CustomEvent');
         event.initCustomEvent("addon-message", true, true, {"detail":{
-    		"command": "delete",
-    		"path": path
-    	}});
+            "command": "delete",
+            "path": path
+        }});
         document.documentElement.dispatchEvent(event);	
     };
     
@@ -213,30 +214,30 @@ define(function(require, exports, module) {
     };
     
     var selectDirectory = function() {
-    	console.log("Selecting directory...");
+        console.log("Selecting directory...");
         var event = document.createEvent('CustomEvent');
         event.initCustomEvent("addon-message", true, true, {"detail":{
-    		"command": "selectDirectory",
-    	}});
+            "command": "selectDirectory"
+        }});
         document.documentElement.dispatchEvent(event);	
     };
     
     var selectFile = function() {
-    	console.log("Selecting file...");
+        console.log("Selecting file...");
         var event = document.createEvent('CustomEvent');
         event.initCustomEvent("addon-message", true, true, {"detail":{
-    		"command": "selectFile"
-    	}});
+            "command": "selectFile"
+        }});
         document.documentElement.dispatchEvent(event);	
     };
         
     var openDirectory = function(dirPath) {
-    	console.log("Opening directory: "+dirPath);
+        console.log("Opening directory: "+dirPath);
         var event = document.createEvent('CustomEvent');
         event.initCustomEvent("addon-message", true, true, {"detail":{
-    		"command": "openDirectory",
-    		"path": dirPath,
-    	}});
+            "command": "openDirectory",
+            "path": dirPath
+        }});
         document.documentElement.dispatchEvent(event);	
     };
     
@@ -276,7 +277,7 @@ define(function(require, exports, module) {
         TSCORE.showLoadingAnimation();   
         var event = document.createEvent('CustomEvent');
         event.initCustomEvent("addon-message", true, true, {"detail":{
-            "command": "checkNewVersion",
+            "command": "checkNewVersion"
         }});
         document.documentElement.dispatchEvent(event);  
     };
@@ -298,24 +299,24 @@ define(function(require, exports, module) {
         TSCORE.showAlertDialog("Open files natively is not implemented on chrome yet!");
     };    
     
-    exports.saveSettings 				= saveSettings;
-    exports.loadSettings 				= loadSettings;
+    exports.saveSettings                = saveSettings;
+    exports.loadSettings                = loadSettings;
     
-	exports.createDirectory 			= createDirectory; 
-	exports.renameFile 					= renameFile;
-	exports.loadTextFile 				= loadTextFile;
-	exports.saveTextFile 				= saveTextFile;
-	exports.listDirectory 				= listDirectory;
-	exports.deleteElement 				= deleteElement;
-    exports.createDirectoryIndex 		= createDirectoryIndex;
-    exports.createDirectoryTree 		= createDirectoryTree;
-	exports.selectDirectory 			= selectDirectory;
-	exports.openDirectory				= openDirectory;
+    exports.createDirectory             = createDirectory;
+    exports.renameFile                  = renameFile;
+    exports.loadTextFile                = loadTextFile;
+    exports.saveTextFile                = saveTextFile;
+    exports.listDirectory               = listDirectory;
+    exports.deleteElement               = deleteElement;
+    exports.createDirectoryIndex        = createDirectoryIndex;
+    exports.createDirectoryTree         = createDirectoryTree;
+    exports.selectDirectory             = selectDirectory;
+    exports.openDirectory               = openDirectory;
     exports.openFile                    = openFile;
-	exports.selectFile 					= selectFile;
-	exports.openExtensionsDirectory 	= openExtensionsDirectory;
-	exports.checkAccessFileURLAllowed 	= checkAccessFileURLAllowed;
-	exports.checkNewVersion 			= checkNewVersion;	
-	exports.getFileProperties           = getFileProperties;    
+    exports.selectFile                  = selectFile;
+    exports.openExtensionsDirectory     = openExtensionsDirectory;
+    exports.checkAccessFileURLAllowed   = checkAccessFileURLAllowed;
+    exports.checkNewVersion             = checkNewVersion;
+    exports.getFileProperties           = getFileProperties;
 
 });
