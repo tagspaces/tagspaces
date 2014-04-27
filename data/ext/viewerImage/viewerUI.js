cef9ee49097254426f56c88c0a7aa1a7401e8ca2
Ilian <ilian@tagspaces.org>
2014-04-27 23:46:43 +0200

    jshinting

--- data/ext/viewerImage/viewerUI.js
+++ data/ext/viewerImage/viewerUI.js
@@ -1,53 +1,52 @@
-/* Copyright (c) 2012-2013 The TagSpaces Authors. All rights reserved.
+/* Copyright (c) 2012-2014 The TagSpaces Authors. All rights reserved.
  * Use of this source code is governed by a AGPL3 license that 
  * can be found in the LICENSE file. */
+/* global define, isCordova */
 
 define(function(require, exports, module) {
 "use strict";
-	
+
 console.log("Loading UI for Image Viewer");
 
-	var TSCORE = require("tscore");
+    var TSCORE = require("tscore");
 
-	var TMB_SIZES = [ "100px", "200px", "300px", "400px", "500px" ];
+    function ExtUI(extID, extContainerID, filePath) {
+        this.extensionID = extID;
+        this.containerElem = $('#'+extContainerID);
 
-	function ExtUI(extID, extContainerID, filePath) {
-		this.extensionID = extID;
-	    this.containerElem = $('#'+extContainerID);
-	    
-	    this.internPath = filePath;
-	    
+        this.internPath = filePath;
+
         //TODO minimize platform specific calls
         if(isCordova) {
             this.filePath = filePath;            
         } else {
             this.filePath = "file:///"+filePath;  
-        }	    
-	}
-	
-	ExtUI.prototype.buildUI = function(uiTemplate) {
-		console.log("Init UI module");
-		
+        }
+    }
+
+    ExtUI.prototype.buildUI = function(uiTemplate) {
+        console.log("Init UI module");
+
         this.viewContainer = $("#"+this.extensionID+"Container").empty();
         this.viewToolbar = $("#"+this.extensionID+"Toolbar").empty();
         this.viewFooter = $("#"+this.extensionID+"Footer").empty();		
-		       
-		var self = this;
-		
+
+        var self = this;
+
         var context = {
-        	id: this.extensionID,
-        	imgPath: this.filePath,
+            id: this.extensionID,
+            imgPath: this.filePath
         };
-		//console.log(uiTemplate(context));
-     	this.containerElem.append(uiTemplate(context));
+        //console.log(uiTemplate(context));
+        this.containerElem.append(uiTemplate(context));
 
         if(isCordova) {
             $("#"+this.extensionID+"ZoomOut").hide();
             $("#"+this.extensionID+"ZoomIn").hide();
         }
 
-    	$("#"+this.extensionID+"imgViewer")
-        	.panzoom({
+        $("#"+this.extensionID+"imgViewer")
+            .panzoom({
                 $zoomIn: $("#"+this.extensionID+"ZoomIn"),
                 $zoomOut: $("#"+this.extensionID+"ZoomOut"),
                 $reset: $("#"+this.extensionID+"ZoomReset"),
@@ -57,23 +56,21 @@ console.log("Loading UI for Image Viewer");
     //            $zoomRange: $section.find(".zoom-range"),
     //            $reset: $section.find(".reset")
             })
-    		.hammer().on("swipeleft", function(event) {
-    			TSCORE.FileOpener.openFile(TSCORE.PerspectiveManager.getNextFile(self.internPath));
-    		})
-    		.hammer().on("swiperight", function(event) {
-    			TSCORE.FileOpener.openFile(TSCORE.PerspectiveManager.getPrevFile(self.internPath));
-    		});         
-
-        $("#"+this.extensionID+"imgViewer")
+            .hammer().on("swipeleft", function() {
+                TSCORE.FileOpener.openFile(TSCORE.PerspectiveManager.getNextFile(self.internPath));
+            })
+            .hammer().on("swiperight", function() {
+                TSCORE.FileOpener.openFile(TSCORE.PerspectiveManager.getPrevFile(self.internPath));
+            })
             .parent().on('mousewheel.focal', function( e ) {
-            	e.preventDefault();
-            	var delta = e.delta || e.originalEvent.wheelDelta;
-            	var zoomOut = delta ? delta < 0 : e.originalEvent.deltaY > 0;
-            	$("#"+self.extensionID+"imgViewer").panzoom('zoom', zoomOut, {
-              		increment: 0.1,
-              		focal: e
-            	});
-          	});	    
+                e.preventDefault();
+                var delta = e.delta || e.originalEvent.wheelDelta;
+                var zoomOut = delta ? delta < 0 : e.originalEvent.deltaY > 0;
+                $("#"+self.extensionID+"imgViewer").panzoom('zoom', zoomOut, {
+                    increment: 0.1,
+                    focal: e
+                });
+            });
 
         // Image Processing
         $("#"+this.extensionID+"Brighter")
@@ -83,15 +80,10 @@ console.log("Loading UI for Image Viewer");
         
         $("#"+this.extensionID+"RotateLeft")
             .click(function() {
-            	console.log("Rotate");
+                console.log("Rotate");
                 $("#"+self.extensionID+"imgViewer").pixastic("rotate", {angle:90});         
-            }); 
-        
-        
-
-  
-
+            });
     };
     
-	exports.ExtUI	 				= ExtUI;
+    exports.ExtUI		= ExtUI;
 });
