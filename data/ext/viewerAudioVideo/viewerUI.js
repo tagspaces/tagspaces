c6ed2cc4496e44da0160a39f844d7db8fd1249a4
Ilian <ilian@tagspaces.org>
2014-05-12 15:30:16 +0200

    adding handling of the web version

--- data/ext/viewerAudioVideo/viewerUI.js
+++ data/ext/viewerAudioVideo/viewerUI.js
@@ -16,7 +16,7 @@ console.log("Loading UI for Image Viewer");
 	    this.containerElem = $('#'+extContainerID);
 
         //TODO minimize platform specific calls
-        if(isCordova) {
+        if(isCordova || isWeb) {
             this.filePath = filePath;            
         } else {
             this.filePath = "file:///"+filePath;  
