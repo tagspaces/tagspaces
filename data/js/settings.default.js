d18b42f8029a99a909af650e3c5848ad6d2b0145
Ilian <ilian@tagspaces.org>
2014-05-26 12:38:01 +0200

    version increased to 1.8.2

--- data/js/settings.default.js
+++ data/js/settings.default.js
@@ -7,8 +7,8 @@ define(function (require, exports, module) {
     exports.defaultSettings = {
         "appName": "TagSpaces",
         "appVersion": "1.8",
-        "appBuild": "1",
-        "appBuildID": "201405140939",
+        "appBuild": "2",
+        "appBuildID": "201405261236",
         "settingsVersion": 2,
         "newTextFileContent": "Created with TagSpaces",
         "newHTMLFileContent": "<html><head><title>Created with TagSpaces</title><meta http-equiv='Content-Type' content='text/html; charset=utf-8'></head><body></body></html>",
