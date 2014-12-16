f7ed1dd3b86d3c9965ff792b2d005cb9c369856e
uggrock <ugg.rock@gmail.com>
2014-12-16 23:09:55 +0100

    adding new popup in chrome for application start

--- data/chromelight/background.js
+++ data/chromelight/background.js
@@ -1,9 +1,9 @@
 /* Copyright (c) 2012-2013 The TagSpaces Authors. All rights reserved.
  * Use of this source code is governed by a AGPL3 license that 
  * can be found in the LICENSE file. */
-chrome.browserAction.onClicked.addListener(function() {
+/*chrome.browserAction.onClicked.addListener(function() {
 	chrome.tabs.create({ url: 'index.html'});
-});
+});*/
 chrome.runtime.onInstalled.addListener(function(details){
     if(details.reason == "install"){
         chrome.tabs.create({url: "index.html"});
@@ -12,3 +12,4 @@ chrome.runtime.onInstalled.addListener(function(details){
         console.log("Updated from " + details.previousVersion + " to " + thisVersion +" !");
     }
 });
+//chrome.runtime.setUninstallURL("http://www.tagspaces.org/uninstallsurvey/");
