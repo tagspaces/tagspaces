c11fda4545289a3a0503dcca928aab35d7ab47cd
uggrock <ugg.rock@gmail.com>
2014-12-24 00:40:29 +0100

    disabling opening of the tagspaces tab on first install

--- data/chromelight/background.js
+++ data/chromelight/background.js
@@ -4,12 +4,12 @@
 /*chrome.browserAction.onClicked.addListener(function() {
 	chrome.tabs.create({ url: 'index.html'});
 });*/
-chrome.runtime.onInstalled.addListener(function(details){
+/*chrome.runtime.onInstalled.addListener(function(details){
     if(details.reason == "install"){
         chrome.tabs.create({url: "index.html"});
     }else if(details.reason == "update"){
         var thisVersion = chrome.runtime.getManifest().version;
         console.log("Updated from " + details.previousVersion + " to " + thisVersion +" !");
     }
-});
+});*/
 //chrome.runtime.setUninstallURL("http://www.tagspaces.org/uninstallsurvey/");
