f4c677d45f0ad42c940722bd7031acdd31f80b2a
uggrock <ugg.rock@gmail.com>
2015-03-04 21:00:40 +0100

    updating the translations, adding czech and turkish

--- default.settings.tmpl.js
+++ default.settings.tmpl.js
@@ -48,6 +48,8 @@ define(function (require, exports, module) {
             { "iso": "es",	    "title": "Español (Spanish)" },
             { "iso": "uk",	    "title": "Український (Ukrainian)" },
             { "iso": "ru",	    "title": "Русский (Russian)" }
+            { "iso": "tr",	    "title": " (Türk)" }
+            { "iso": "cs",	    "title": " (Čeština)" }
         ],
         "keyBindings": {
             "closeViewer":              "mod+w",
