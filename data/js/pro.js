b1ed648fe94c7c8113cc8c80e782b3e79d02556e
uggrock <ugg.rock@gmail.com>
2015-04-17 21:39:31 +0200

    add use strict

--- data/js/pro.js
+++ data/js/pro.js
@@ -2,6 +2,7 @@
  * Use of this source code is governed by a AGPL3 license that
  * can be found in the LICENSE file. */
 define(function(require, exports, module) {
+  'use strict';
   console.log("Pro not available");
 
   exports.available = false;
