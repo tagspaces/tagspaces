031fc5a8ba5f88f05a6afab407955ee02db3d497
uggrock <ugg.rock@gmail.com>
2015-10-05 22:01:28 +0200

    adding test error handling

--- data/web/web.api.js
+++ data/web/web.api.js
@@ -234,7 +234,11 @@ define(function(require, exports, module) {
       encodeURI(path),
       function(status, data, headers) {
         console.log("Directory/File Deletion Status/Content/Headers:  " + status + " / " + data + " / " + headers);
-        TSPOSTIO.deleteElement(path);
+        if(status === 204) {
+          TSPOSTIO.deleteElement(path);
+        } else {
+          TSCORE.showAlertDialog("Deletion of the file failed");
+        }
       }
     );
   };
