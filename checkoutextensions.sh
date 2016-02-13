147b4e2e558130842e0f5b4f81ffec68b074d2bc
mladensap <mladen@sapundshiev.de>
2016-02-13 16:03:32 +0100

    adding script for checkouting extensions

---
+++ checkoutextensions.sh
@@ -0,0 +1,304 @@
+
+#!/bin/bash
+if [ ! -d "extension" ]; then
+    mkdir extension fi
+
+
+# -------------------------
+# perspectveGrid
+# -------------------------
+if [ -d "extensions/perspectiveGrid" ]; then
+    cd extensions/perspectiveGrid
+    git fetch --all
+    cd ../..
+else
+    git clone https://github.com/tagspaces/perspectiveGrid.git extensions/perspectiveGrid
+fi
+
+#!/bin/bash
+if [ ! -d "extension" ]; then
+    mkdir extension fi
+
+
+# -------------------------
+# perspectiveImageSwiper
+# -------------------------
+if [ -d "extensions/perspectiveImageSwiper" ]; then
+    cd extensions/perspectiveImageSwiper
+    git fetch --all
+    cd ../..
+else
+    git clone https://github.com/tagspaces/perspectiveImageSwiper.git extensions/perspectiveImageSwiper
+fi
+
+#!/bin/bash
+if [ ! -d "extension" ]; then
+    mkdir extension fi
+
+
+# -------------------------
+# perspectiveList
+# -------------------------
+if [ -d "extensions/perspectiveList" ]; then
+    cd extensions/perspectiveList
+    git fetch --all
+    cd ../..
+else
+    git clone https://github.com/tagspaces/perspectiveList.git extensions/perspectiveList
+fi
+
+#!/bin/bash
+if [ ! -d "extension" ]; then
+    mkdir extension fi
+
+
+# -------------------------
+# perspectiveGraph
+# -------------------------
+if [ -d "extensions/perspectiveGraph" ]; then
+    cd extensions/perspectiveGraph
+    git fetch --all
+    cd ../..
+else
+    git clone https://github.com/tagspaces/perspectiveGraph.git extensions/perspectiveGraph
+fi
+
+#!/bin/bash
+if [ ! -d "extension" ]; then
+    mkdir extension fi
+
+
+# -------------------------
+# viewerHTML
+# -------------------------
+if [ -d "extensions/viewerHTML" ]; then
+    cd extensions/perspectiveImageSwiper
+    git fetch --all
+    cd ../..
+else
+    git clone https://github.com/tagspaces/viewerHTML.git extensions/viewerHTML
+fi
+
+#!/bin/bash
+if [ ! -d "extension" ]; then
+    mkdir extension fi
+
+
+# -------------------------
+# viewerImage
+# -------------------------
+if [ -d "extensions/viewerImage" ]; then
+    cd extensions/viewerImage
+    git fetch --all
+    cd ../..
+else
+    git clone https://github.com/tagspaces/viewerImage.git extensions/viewerImage
+fi
+
+#!/bin/bash
+if [ ! -d "extension" ]; then
+    mkdir extension fi
+
+
+# -------------------------
+# viewerMD
+# -------------------------
+if [ -d "extensions/viewerMD" ]; then
+    cd extensions/viewerMD
+    git fetch --all
+    cd ../..
+else
+    git clone https://github.com/tagspaces/viewerMD.git extensions/viewerMD
+fi
+
+#!/bin/bash
+if [ ! -d "extension" ]; then
+    mkdir extension fi
+
+
+# -------------------------
+# viewerMHTML
+# -------------------------
+if [ -d "extensions/viewerMHTML" ]; then
+    cd extensions/viewerMHTML
+    git fetch --all
+    cd ../..
+else
+    git clone https://github.com/tagspaces/viewerMHTML.git extensions/viewerMHTML
+fi
+
+#!/bin/bash
+if [ ! -d "extension" ]; then
+    mkdir extension fi
+
+
+# -------------------------
+# viewerURL
+# -------------------------
+if [ -d "extensions/viewerURL" ]; then
+    cd extensions/viewerURL
+    git fetch --all
+    cd ../..
+else
+    git clone https://github.com/tagspaces/viewerURL.git extensions/viewerURL
+fi
+
+#!/bin/bash
+if [ ! -d "extension" ]; then
+    mkdir extension fi
+
+
+# -------------------------
+# viewerZIP
+# -------------------------
+if [ -d "extensions/viewerZIP" ]; then
+    cd extensions/viewerZIP
+    git fetch --all
+    cd ../..
+else
+    git clone https://github.com/tagspaces/viewerZIP.git extensions/viewerZIP
+fi
+
+#!/bin/bash
+if [ ! -d "extension" ]; then
+    mkdir extension fi
+
+
+# -------------------------
+# viewerPDF
+# -------------------------
+if [ -d "extensions/viewerPDF" ]; then
+    cd extensions/viewerPDF
+    git fetch --all
+    cd ../..
+else
+    git clone https://github.com/tagspaces/viewerPDF.git extensions/viewerPDF
+fi
+
+#!/bin/bash
+if [ ! -d "extension" ]; then
+    mkdir extension fi
+
+
+# -------------------------
+# viewerText
+# -------------------------
+if [ -d "extensions/viewerText" ]; then
+    cd extensions/viewerText
+    git fetch --all
+    cd ../..
+else
+    git clone https://github.com/tagspaces/viewerText.git extensions/viewerText
+fi
+
+#!/bin/bash
+if [ ! -d "extension" ]; then
+    mkdir extension fi
+
+
+# -------------------------
+# editorHTML
+# -------------------------
+if [ -d "extensions/editorHTML" ]; then
+    cd extensions/editorHTML
+    git fetch --all
+    cd ../..
+else
+    git clone https://github.com/tagspaces/editorHTML.git extensions/editorHTML
+fi
+
+#!/bin/bash
+if [ ! -d "extension" ]; then
+    mkdir extension fi
+
+
+# -------------------------
+# editorJSONv
+# -------------------------
+if [ -d "extensions/editorJSON" ]; then
+    cd extensions/editorJSON
+    git fetch --all
+    cd ../..
+else
+    git clone https://github.com/tagspaces/editorJSON.git extensions/editorJSON
+fi
+
+#!/bin/bash
+if [ ! -d "extension" ]; then
+    mkdir extension fi
+
+
+# -------------------------
+# editorODF
+# -------------------------
+if [ -d "extensions/editorODF" ]; then
+    cd extensions/editorODF
+    git fetch --all
+    cd ../..
+else
+    git clone https://github.com/tagspaces/editorODF.git extensions/editorODF
+fi
+
+#!/bin/bash
+if [ ! -d "extension" ]; then
+    mkdir extension fi
+
+
+# -------------------------
+# editorText
+# -------------------------
+if [ -d "extensions/editorText" ]; then
+    cd extensions/editorText
+    git fetch --all
+    cd ../..
+else
+    git clone https://github.com/tagspaces/editorText.git extensions/editorText
+fi
+
+#!/bin/bash
+if [ ! -d "extension" ]; then
+    mkdir extension fi
+
+
+# -------------------------
+# viewerEPUB
+# -------------------------
+if [ -d "extensions/viewerEPUB" ]; then
+    cd extensions/viewerEPUB
+    git fetch --all
+    cd ../..
+else
+    git clone https://github.com/tagspaces/viewerEPUB.git extensions/viewerEPUB
+fi
+
+#!/bin/bash
+if [ ! -d "extension" ]; then
+    mkdir extension fi
+
+
+# -------------------------
+# viewerAudioVideo
+# -------------------------
+if [ -d "extensions/viewerAudioVideo" ]; then
+    cd extensions/viewerAudioVideo
+    git fetch --all
+    cd ../..
+else
+    git clone https://github.com/tagspaces/viewerAudioVideo.git extensions/viewerAudioVideo
+fi
+
+#!/bin/bash
+if [ ! -d "extension" ]; then
+    mkdir extension fi
+
+
+# -------------------------
+# viewerBrowser
+# -------------------------
+if [ -d "extensions/viewerBrowser" ]; then
+    cd extensions/viewerBrowser
+    git fetch --all
+    cd ../..
+else
+    git clone https://github.com/tagspaces/viewerBrowser.git extensions/viewerBrowser
+fi
