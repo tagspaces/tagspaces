9435d3c018060c3e3ab00cfb0ebce53e793765ea
uggrock <ugg.rock@gmail.com>
2016-03-06 10:03:53 +0100

    adding npm command for running a lockal test webdav server

--- data/web/webdavserver.js
+++ data/web/webdavserver.js
@@ -5,7 +5,7 @@ jsDAV.debugMode = true;
 var jsDAV_Auth_Backend_File = require("jsDAV/lib/DAV/plugins/auth/file");
 
 jsDAV.createServer({
-    node: "./data",
+    node: "../../data",
     authBackend:  jsDAV_Auth_Backend_File.new("./jsdavauth"),
     realm: "jdavtest"
 }, 8000);
