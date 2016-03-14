562282d22f426b5b1dd3adf1e4426cc57e21e22d
uggrock <ugg.rock@gmail.com>
2016-03-14 18:11:07 +0100

    adding extra osx menu

--- data/electron/electron.api.js
+++ data/electron/electron.api.js
@@ -156,6 +156,62 @@ define(function(require, exports, module) {
       },
     ];
 
+    if (process.platform == 'darwin') {
+      var name = "TagSpaces";
+      template.unshift({
+        label: name,
+        submenu: [
+          {
+            label: 'About ' + name,
+            role: 'about'
+          },
+          {
+            type: 'separator'
+          },
+          {
+            label: 'Services',
+            role: 'services',
+            submenu: []
+          },
+          {
+            type: 'separator'
+          },
+          {
+            label: 'Hide ' + name,
+            accelerator: 'Command+H',
+            role: 'hide'
+          },
+          {
+            label: 'Hide Others',
+            accelerator: 'Command+Alt+H',
+            role: 'hideothers'
+          },
+          {
+            label: 'Show All',
+            role: 'unhide'
+          },
+          {
+            type: 'separator'
+          },
+          {
+            label: 'Quit',
+            accelerator: 'Command+Q',
+            click: function() { app.quit(); }
+          },
+        ]
+      });
+      // Window menu.
+      /*template[3].submenu.push(
+        {
+          type: 'separator'
+        },
+        {
+          label: 'Bring All to Front',
+          role: 'front'
+        }
+      );*/
+    }
+
     var menu = Menu.buildFromTemplate(template);
     Menu.setApplicationMenu(menu); 
   }
@@ -188,7 +244,7 @@ define(function(require, exports, module) {
         type: 'GET'
       })
       .done(function(data) {
-        TSPOSTIO.checkNewVersion(data);
+        TSCORE.updateNewVersionData(data);
       })
       .fail(function(data) {
         console.log("AJAX failed " + data);
