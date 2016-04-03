cf0ee8584b71178eedf850a256fe5ae8a7d9ec19
uggrock <ugg.rock@gmail.com>
2016-04-03 11:12:35 +0200

    js styling

--- data/electron/electron.api.js
+++ data/electron/electron.api.js
@@ -6,7 +6,7 @@ const fs = require('fs-extra'); // jshint ignore:line
 const pathUtils = require('path'); // jshint ignore:line
 const electron = require('electron'); // jshint ignore:line
 const remote = electron.remote; // jshint ignore:line
-const ipcRenderer = require('electron').ipcRenderer;
+const ipcRenderer = require('electron').ipcRenderer; // jshint ignore:line
 
 /**
  * A implementation of the IOAPI for the electron platform
