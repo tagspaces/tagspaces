6585364c5f55b2901b243a63e615ba694f1e2f26
kristiyandd <kristiyandd@gmail.com>
2016-06-23 20:07:33 +0300

    ui

--- data/loader.js
+++ data/loader.js
@@ -107,6 +107,9 @@ var isWin = navigator.appVersion.indexOf("Win")!==-1;
       noty:                   'libs/noty/js/noty/packaged/jquery.noty.packaged.min',
       marked:                 'libs/marked/marked.min',
       webaudiorecording:      'libs/audio-record/WebAudioRecorder',
+      offlinelib:             'web/offlinelib/offline.min',
+      webdav:                 'web/webdavlib/webdavlib',
+      pickr:                  'libs/pickr/flatpickr.min',
 
       tscore:                 'js/core.api',
       tssetting:              'js/settings.api',
