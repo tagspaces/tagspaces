7da060942a3531101cb8838147196e5576134b52
kristiyandd <kristiyandd@gmail.com>
2016-09-15 14:08:37 +0300

    update: transfer functions

--- data/loader.js
+++ data/loader.js
@@ -53,7 +53,7 @@ var isWin = navigator.appVersion.indexOf("Win")!==-1;
     console.log(e.message);
   }
   var PRO_JS = "pro/js/pro.api";
-  if(PRO.indexOf("@@PROVERS") == 0 || PRO == "false") { PRO_JS = 'js/pro'; }
+  //if(PRO.indexOf("@@PROVERS") == 0 || PRO == "false") { PRO_JS = 'js/pro'; }
 
   // Setting up the IO functionality according to the platform
   var IO_JS = "web/web.api";
@@ -109,7 +109,6 @@ var isWin = navigator.appVersion.indexOf("Win")!==-1;
       webaudiorecording:      'libs/audio-record/WebAudioRecorder',
       offlinelib:             'web/offlinelib/offline.min',
       webdav:                 'web/webdavlib/webdavlib',
-      leaflet:                'libs/leaflet/dist/leaflet',
       moment:                 'libs/moment/min/moment.min',
       momenttimezone:         'libs/moment-timezone/builds/moment-timezone.min',
       datetimepicker:         'libs/eonasdan-bootstrap-datetimepicker/build/js/bootstrap-datetimepicker.min',
