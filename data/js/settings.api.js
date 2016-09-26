6eb88fcafd7ef7f1b7a0ac782c61d51da538dde2
kristiyandd <kristiyandd@gmail.com>
2016-09-26 14:38:12 +0300

    fix edit color in groups

--- data/js/settings.api.js
+++ data/js/settings.api.js
@@ -1098,6 +1098,8 @@ define(function(require, exports, module) {
     for (var i = 0; i < exports.Settings.tagGroups.length; i++) {
       if (exports.Settings.tagGroups[i].key === tagData.key) {
         exports.Settings.tagGroups[i].title = tagGroupName;
+        exports.Settings.tagGroups[i].color = tagGroupColor;
+        exports.Settings.tagGroups[i].textcolor = tagGroupTextColor;
         for (var j = 0; j < exports.Settings.tagGroups[i].children.length; j++) {
           //if (exports.Settings.tagGroups[i].children[j].color !== tagData.color) {
             exports.Settings.tagGroups[i].children[j].color = tagGroupColor;
