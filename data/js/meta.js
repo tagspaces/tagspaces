2478f1bdeabe0e6ebf6990e4d759eec2eb602f77
uggrock <ugg.rock@gmail.com>
2017-01-24 15:15:19 +0100

    updating the copyright text plus other small adjustments

--- data/js/meta.js
+++ data/js/meta.js
@@ -1,4 +1,4 @@
-/* Copyright (c) 2012-2016 The TagSpaces Authors. All rights reserved.
+/* Copyright (c) 2012-2017 The TagSpaces Authors. All rights reserved.
  * Use of this source code is governed by a AGPL3 license that
  * can be found in the LICENSE file. */
 
