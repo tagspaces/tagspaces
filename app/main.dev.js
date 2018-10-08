f592852579652520c586661cb73790091022acb6
Ilian Sapundshiev <ilian@tagspaces.org>
2018-10-08 12:10:02 +0200

    fixing issue with closing the app on mac

--- app/main.dev.js
+++ app/main.dev.js
@@ -79,9 +79,9 @@ const installExtensions = async () => {
 app.on('window-all-closed', () => {
   // Respect the OSX convention of having the application in memory even
   // after all windows have been closed
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
+  // if (process.platform !== 'darwin') {
+  app.quit();
+  // }
 });
 
 app.on('ready', async () => {
