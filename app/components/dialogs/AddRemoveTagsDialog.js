7a25054e44250cfe2c7a44462104dc8c7d509c14
Ilian Sapundshiev <ilian@tagspaces.org>
2018-11-14 17:47:30 +0100

    cleaning up

--- app/components/dialogs/AddRemoveTagsDialog.js
+++ app/components/dialogs/AddRemoveTagsDialog.js
@@ -17,8 +17,7 @@
  * @flow
  */
 
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import React from 'react';
 import Button from '@material-ui/core/Button';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
