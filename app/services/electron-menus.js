ac7bde80c23f496e2edba85ad20ce0c2d13258e3
Jason Penny <jpenny@tumblr.com>
2018-11-13 20:43:05 -0500

    Allow Cmd+Q to quit when running on a mac

--- app/services/electron-menus.js
+++ app/services/electron-menus.js
@@ -129,7 +129,7 @@ export default function buildDesktopMenu(mainPageProps: Object) {
         }, */
         {
           label: i18n.t('core:exitApp'),
-          accelerator: '',
+          accelerator: (process.platform === 'darwin') ? 'Cmd+Q' : '',
           click: () => {
             ipcRenderer.send('quit-application', 'Bye, bye...');
           }
