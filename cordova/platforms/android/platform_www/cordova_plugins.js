71104812adff6ffec41dfae9599c937c65d63fed
Stanimir Marinov <s.marinov@structmedia.com>
2018-12-17 21:49:12 +0200

    install whitelist plugin

--- cordova/platforms/android/platform_www/cordova_plugins.js
+++ cordova/platforms/android/platform_www/cordova_plugins.js
@@ -186,7 +186,8 @@ module.exports.metadata =
 // TOP OF METADATA
 {
   "cordova-plugin-file": "6.0.1",
-  "cordova-plugin-device": "2.0.2"
+  "cordova-plugin-device": "2.0.2",
+  "cordova-plugin-whitelist": "1.3.3"
 };
 // BOTTOM OF METADATA
 });
