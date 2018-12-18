db835e1037ab6e6f2a6bb739ac235c3372d5b619
Ilian Sapundshiev <ilian@tagspaces.org>
2018-12-18 14:33:13 +0100

    fix text file loading in worker

--- webpack.config.worker.prod.js
+++ webpack.config.worker.prod.js
@@ -105,6 +105,11 @@ export default merge.smart(baseConfig, {
           }
         }]
       },
+      // Text files
+      {
+        test: /\.(txt)$/,
+        use: 'raw-loader'
+      },
       // Common Image Formats
       {
         test: /\.(?:ico|gif|png|jpg|jpeg|webp)$/,
