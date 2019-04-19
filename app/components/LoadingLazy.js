efa435b33c77e75fd188bb0f2939b937fecae0cf
Ilian Sapundshiev <ilian@tagspaces.org>
2019-04-19 16:03:14 +0200

    minimizing the lazyloading component

--- app/components/LoadingLazy.js
+++ app/components/LoadingLazy.js
@@ -20,6 +20,7 @@
 import React from 'react';
 // import i18n from '../services/i18n';
 // {i18n.t('core:loading')}
+// <div style={{ padding: 10, backgroundColor: 'transparent' }}>...</div>
 
-const LoadingLazy = () => (<div style={{ padding: 10, backgroundColor: 'transparent' }}>...</div>);
+const LoadingLazy = () => (<div style={{ width: 1, height: 1, backgroundColor: 'transparent' }}>...</div>);
 export default LoadingLazy;
