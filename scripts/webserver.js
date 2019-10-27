ac2f7e98132a8a198fdc746f3fcf2e12b64bd508
Ilian Sapundshiev <ilian@tagspaces.org>
2019-10-27 20:57:29 +0100

    adding static website hosting for testing purposes

---
+++ scripts/webserver.js
@@ -0,0 +1,8 @@
+var express = require('express')
+var serveStatic = require('serve-static')
+const port = 8000
+var app = express()
+
+app.use(serveStatic('./web', { 'index': ['index.html'] }))
+app.listen(port)
+console.log('Webserver listining on http://127.0.0.1:' + port)
