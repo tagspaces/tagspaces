58301de258dbfd20dd9be96ecd093588e5e41bea
Ilian Sapundshiev <ilian@tagspaces.org>
2019-11-04 15:23:33 +0100

    showing hostname for the PWA

--- web/pwa.js
+++ web/pwa.js
@@ -1,7 +1,9 @@
 'use strict';
+
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
     navigator.serviceWorker.register('/sw.js').then((reg) => {
+      document.title = 'TagSpaces PWA - ' + document.location.hostname;
       // console.log('TagSpaces service worker registered.', reg);
       return true;
     }).catch(err => console.warn('Error registering TagSpaces service worker ' + err));
