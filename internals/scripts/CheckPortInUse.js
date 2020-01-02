7939f352c50bc24afeabf0d5360541393534371d
Ilian Sapundshiev <ilian@tagspaces.org>
2020-01-02 16:24:37 +0100

    typescript integration

---
+++ internals/scripts/CheckPortInUse.js
@@ -0,0 +1,18 @@
+import chalk from 'chalk';
+import detectPort from 'detect-port';
+
+(function CheckPortInUse() {
+  const port = process.env.PORT || '1212';
+
+  detectPort(port, (err, availablePort) => {
+    if (port !== String(availablePort)) {
+      throw new Error(
+        chalk.whiteBright.bgRed.bold(
+          `Port "${port}" on "localhost" is already in use. Please use another port. ex: PORT=4343 yarn dev`
+        )
+      );
+    } else {
+      process.exit(0);
+    }
+  });
+})();
