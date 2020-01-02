0d94c33d69e211f358550bc4fdc4ec1f22a55e56
Ilian Sapundshiev <ilian@tagspaces.org>
2020-01-02 17:33:46 +0100

    boilerplace integration

---
+++ configs/webpack.config.base.js
@@ -0,0 +1,50 @@
+/**
+ * Base webpack config used across other specific configs
+ */
+
+import path from 'path';
+import webpack from 'webpack';
+import { dependencies } from '../package.json';
+
+export default {
+  externals: [...Object.keys(dependencies || {})],
+
+  module: {
+    rules: [
+      {
+        test: /\.[jt]sx?$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              cacheDirectory: true
+            }
+          },
+          'ts-loader'
+        ]
+      }
+    ]
+  },
+
+  output: {
+    path: path.join(__dirname, '..', 'app'),
+    // https://github.com/webpack/webpack/issues/1114
+    libraryTarget: 'commonjs2'
+  },
+
+  /**
+   * Determine the array of extensions that should be used to resolve modules.
+   */
+  resolve: {
+    extensions: ['.js', '.ts', '.tsx', '.json']
+  },
+
+  plugins: [
+    new webpack.EnvironmentPlugin({
+      NODE_ENV: 'production'
+    }),
+
+    new webpack.NamedModulesPlugin()
+  ]
+};
