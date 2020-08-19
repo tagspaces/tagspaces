70ad56f43537b8bc0b1848112320d358716def43
stanimir <s.marinov@structmedia.com>
2020-08-19 20:16:35 +0300

    add unit tests and mock search

---
+++ tests/unit/search.test.js
@@ -0,0 +1,21 @@
+import Search from '../../app/services/search';
+
+const mockedSearchIndex = { data: {} };
+const SearchMock = jest.fn();
+SearchMock.mockReturnValue(Promise.resolve(mockedSearchIndex));
+Search.searchLocationIndex = SearchMock;
+
+jest.mock('../../app/services/search');
+
+test('calls Search.searchLocationIndex with the params', () => {
+  const locationContent = [];
+  const searchQuery = {
+    tagsAND: ['tag'],
+    maxSearchResults: 1
+  };
+
+  expect(
+    Search.searchLocationIndex(locationContent, searchQuery)
+  ).resolves.toStrictEqual(mockedSearchIndex);
+  expect(SearchMock).toHaveBeenCalledWith(locationContent,searchQuery);
+});
