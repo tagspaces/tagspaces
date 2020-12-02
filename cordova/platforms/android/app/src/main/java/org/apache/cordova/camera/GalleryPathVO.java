8727ca4071e637da869b0e8cb3c3d1011d808d39
stanimir <s.marinov@structmedia.com>
2020-12-02 14:59:21 +0200

    cordova android updates

--- cordova/platforms/android/app/src/main/java/org/apache/cordova/camera/GalleryPathVO.java
+++ cordova/platforms/android/app/src/main/java/org/apache/cordova/camera/GalleryPathVO.java
@@ -18,4 +18,26 @@
 */
 package org.apache.cordova.camera;
 
-public class FileProvider extends androidx.core.content.FileProvider {}
+public class GalleryPathVO {
+    private final String galleryPath;
+    private String picturesDirectory;
+    private String galleryFileName;
+
+    public GalleryPathVO(String picturesDirectory, String galleryFileName) {
+        this.picturesDirectory = picturesDirectory;
+        this.galleryFileName = galleryFileName;
+        this.galleryPath = this.picturesDirectory + "/" + this.galleryFileName;
+    }
+
+    public String getGalleryPath() {
+        return galleryPath;
+    }
+
+    public String getPicturesDirectory() {
+        return picturesDirectory;
+    }
+
+    public String getGalleryFileName() {
+        return galleryFileName;
+    }
+}
