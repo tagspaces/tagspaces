88b7d08f7504b9b0b7b3c1d905633c81f56619bb
stanimir <s.marinov@structmedia.com>
2020-12-10 12:42:00 +0200

    TST0801 - Arrow keys select next/prev file (keybindings)

--- tests/e2e/fileopener.e2e.js
+++ tests/e2e/fileopener.e2e.js
@@ -3,9 +3,10 @@ import {
   createLocation,
   createMinioLocation,
   defaultLocationName,
-  defaultLocationPath
+  defaultLocationPath,
+  getPropertiesFileName
 } from './location.helpers';
-import { clickOn, setSettings } from './general.helpers';
+import { clickOn, getGridFileName, setSettings } from './general.helpers';
 import { AddRemoveTagsToFile } from './file.properties.helpers';
 import { searchEngine } from './search.spec';
 import { firstFile, perspectiveGridTable } from './test-utils.spec';
@@ -21,6 +22,24 @@ describe('TST08 - File / folder properties', () => {
     await clickOn('[data-tid=location_' + defaultLocationName + ']');
     // If its have opened file
     await closeFileProperties();
+  });
+
+  it('TST0801 - Arrow keys select next/prev file (keybindings) [TST0801,web,minio,electron]', async () => {
+    const firstFileName = await getGridFileName(0);
+
+    // open fileProperties
+    await clickOn(perspectiveGridTable + firstFile);
+    //Toggle Properties
+    await clickOn('[data-tid=fileContainerToggleProperties]');
+
+    const propsFileName = await getPropertiesFileName();
+    expect(firstFileName).toBe(propsFileName);
+
+    await global.client.keys('ArrowDown');
+    const propsNextFileName = await getPropertiesFileName();
+
+    const secondFileName = await getGridFileName(1);
+    expect(secondFileName).toBe(propsNextFileName);
   });
 
   it('TST0808 - Add and remove tags to a file (file names) [TST0808,web,minio,electron]', async () => {
