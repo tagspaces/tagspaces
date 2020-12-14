b444bbf42973ac7d55feb6cc0ac452073d4cf4b2
Stanimir <stanimir.marinov@structmedia.com>
2020-12-14 15:01:50 +0200

    fix

--- tests/e2e/fileopener.e2e.js
+++ tests/e2e/fileopener.e2e.js
@@ -133,7 +133,7 @@ describe('TST08 - File / folder properties', () => {
     expect(propsOldFileName).toBe(propsFileName);
   });
 
-  it('TST0807 - Rename opened folder [TST0807,web,minio,electron]', async () => {
+  it('TST0807 - Rename opened folder [TST0807,electron]', async () => {
     const newTile = 'folderRenamed';
     // open folderProperties
     await openContextEntryMenu(
