ae6d9293b56c2a5fa89d6afd64a6ce0446ab321a
mladensap <mladen@sapundshiev.de>
2021-01-20 21:20:53 +0200

    TSN test unit - renaming testEntries.json to testEntries.js

--- tests/unit/search.test.js
+++ tests/unit/search.test.js
@@ -1,5 +1,5 @@
 import Search from '../../app/services/search';
-// import searchdata from '-/testEntries.json';
+import { entry1, entry2, entry3 } from './testEntries';
 
 /*const mockedSearchIndex = { data: {} };
 const SearchMock = jest.fn();
@@ -21,59 +21,9 @@ test('calls Search.searchLocationIndex with the params', () => {
   expect(SearchMock).toHaveBeenCalledWith(locationContent, searchQuery);
 });*/
 
-/*const searchQuery: SearchQuery = {
-  textQuery: this.state.textQuery,
-  tagsAND: this.state.tagsAND,
-  tagsOR: this.state.tagsOR,
-  tagsNOT: this.state.tagsNOT,
-  // @ts-ignore
-  searchBoxing: this.state.searchBoxing,
-  fileTypes: this.state.fileTypes,
-  lastModified: this.state.lastModified,
-  fileSize: this.state.fileSize,
-  tagTimePeriodFrom: this.state.tagTimePeriodFrom
-      ? this.state.tagTimePeriodFrom.getTime()
-      : null,
-  tagTimePeriodTo: this.state.tagTimePeriodTo
-      ? this.state.tagTimePeriodTo.getTime()
-      : null,
-  tagPlaceLat: this.state.tagPlaceLat,
-  tagPlaceLong: this.state.tagPlaceLong,
-  tagPlaceRadius: this.state.tagPlaceRadius,
-  maxSearchResults: this.props.maxSearchResults,
-  currentDirectory: this.props.currentDirectory,
-  forceIndexing: this.state.forceIndexing
-};*/
-
 const tag1 = { title: 'tagTitle1' };
 const tag2 = { title: 'tagTitle2' };
 const tag3 = { title: 'tagTitle3' };
-
-const entry1 = {
-  name: 'entryTitle1',
-  description: 'test description',
-  isFile: true,
-  extension: 'jpg',
-  tags: [tag1],
-  fileSize: 111,
-  path: '/gg/'
-};
-
-const entry2 = {
-  name: 'entryTitle2',
-  isFile: true,
-  extension: 'jpg',
-  tags: [tag2],
-  fileSize: 222,
-  path: '/gg/'
-};
-
-const entry3 = {
-  name: 'testFolder1',
-  description: 'test folder desciption',
-  isDirectory: true,
-  path: '/gg/'
-};
 
 // test('calls Search.searchLocationIndex for tags', () => {
 //   const locationContent = [entry1, entry2]; //enhanceEntry(entry)];
@@ -112,6 +62,19 @@ test('calls Search.searchLocationIndex for tags', () => {
   expect(
     Search.searchLocationIndex(locationContent, searchQuery)
   ).resolves.toStrictEqual([entry1]);
+});
+
+test('calls Search.searchLocationIndex for tags with not to equal', () => {
+  const locationContent = [entry1, entry2];
+
+  const searchQuery = {
+    tagsAND: [tag1],
+    maxSearchResults: 2
+  };
+
+  expect(
+    Search.searchLocationIndex(locationContent, searchQuery)
+  ).resolves.not.toStrictEqual([entry2]);
 });
 
 test('calls Search.searchLocationIndex for not exist tags', () => {
---
+++ tests/unit/testEntries.js
@@ -0,0 +1,27 @@
+export const entry1 = {
+  name: 'entryTitle1',
+  description: 'test description',
+  isFile: true,
+  extension: 'jpg',
+  tags: ['tag1'],
+  textConten: 'adsadas dasda',
+  fromTime: '',
+  toTime: '',
+  fileSize: 111
+};
+
+export const entry2 = {
+  name: 'entryTitle2',
+  isFile: true,
+  extension: 'jpg',
+  tags: ['tag2'],
+  fileSize: 222,
+  path: '/gg/'
+};
+
+export const entry3 = {
+  name: 'testFolder1',
+  description: 'test folder desciption',
+  isDirectory: true,
+  path: '/gg/'
+};
