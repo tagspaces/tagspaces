ebb06280df8d7d83be749320443be3befce2ccc9
Ilian Sapundshiev <ilian@tagspaces.org>
2021-01-28 23:14:01 +0100

    fix: issues with editing locations

--- tests/e2e/location.helpers.js
+++ tests/e2e/location.helpers.js
@@ -60,7 +60,7 @@ export async function createMinioLocation(
   // Check if location not exist (from extconfig.js)
   if (locationName !== lastLocationTID) {
     await clickOn('[data-tid=createNewLocation]');
-    await clickOn('[data-tid=objectStorageLocation]');
+    // await clickOn('[data-tid=objectStorageLocation]');
     await clickOn('[data-tid=switchAdvancedModeTID]');
 
     // SET LOCATION NAME
