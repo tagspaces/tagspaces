5d362dfe605dd12de0793241140cbe6ac4455f72
stanimir <s.marinov@structmedia.com>
2021-02-02 14:51:58 +0200

    set Thumbnails function

--- amplify/backend/function/createTagspaceIndex/src/index.js
+++ amplify/backend/function/createTagspaceIndex/src/index.js
@@ -1,17 +1,23 @@
 /* Amplify Params - DO NOT EDIT
 	ENV
 	REGION
-Amplify Params - DO NOT EDIT */const { indexer, persistIndex } = require('indexer');
+Amplify Params - DO NOT EDIT */
 
-exports.handler = async (event, context) => {
+const { indexer, persistIndex } = require('tsindexer');
+const { thumbnailsHandler } = require('ts-thumbnails');
+
+exports.handler = async (event, context, callback) => {
   console.log('Received event:', JSON.stringify(event, null, 2));
   const bucketName = event.Records[0].s3.bucket.name;
-  const path = "/";
+  const { key } = event.Records[0].s3.object;
+  const path = '/';
   // const { path, bucketName } = event;
   const directoryIndex = await indexer(path, bucketName);
   const success = await persistIndex(path, directoryIndex, bucketName);
+
+  thumbnailsHandler(bucketName, key, callback);
   if (success) {
-    console.log('createIndex: ' + JSON.stringify(directoryIndex));
+    // console.log('createIndex: ' + JSON.stringify(directoryIndex));
     context.succeed({});
   }
 };
