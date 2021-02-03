126b39694c2978f284264b10dbe06ab128895b38
stanimir <s.marinov@structmedia.com>
2021-02-03 21:22:27 +0200

    dynamic import aws-config

--- app/containers/Root.tsx
+++ app/containers/Root.tsx
@@ -19,20 +19,12 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
-import { Amplify } from 'aws-amplify';
-import {
-  AmplifyAuthenticator,
-  AmplifySignIn,
-  AmplifySignUp
-} from '@aws-amplify/ui-react';
 import LoadingScreen from '../components/LoadingScreen';
 import { actions as AppActions } from '../reducers/app';
 import AppConfig from '-/config';
 import App from '-/containers/App';
 import MainPage from '-/containers/MainPage';
-import awsconfig from '-/aws-exports';
-import LogoIcon from '-/assets/images/icon100x100.svg';
-import HandleAuth from '-/utils/HandleAuth';
+import Auth from '-/containers/Auth';
 
 type RootType = {
   store: {};
@@ -47,50 +39,7 @@ export default function Root({ store, persistor }: RootType) {
   );
 
   if (AppConfig.isWeb) {
-    Amplify.configure(awsconfig);
-    appContent = (
-      <>
-        <HandleAuth />
-        <AmplifyAuthenticator
-          usernameAlias="email"
-          style={{
-            // @ts-ignore
-            '--amplify-primary-color': '#1dd19f',
-            '--amplify-primary-tint': '#1dd19f',
-            '--amplify-primary-shade': '#4A5568'
-          }}
-        >
-          <AmplifySignUp
-            slot="sign-up"
-            usernameAlias="email"
-            formFields={[
-              {
-                type: 'email',
-                label: 'Email',
-                placeholder: 'Enter your email',
-                required: true
-              },
-              {
-                type: 'password',
-                label: 'Password',
-                placeholder: 'Enter your password',
-                required: true
-              }
-            ]}
-          />
-          <AmplifySignIn
-            headerText="Sign in to your TagSpaces account"
-            slot="sign-in"
-            usernameAlias="email"
-          >
-            <div slot="header-subtitle" style={{ textAlign: 'center' }}>
-              <img alt="logo" src={LogoIcon} />
-            </div>
-          </AmplifySignIn>
-          {appContent}
-        </AmplifyAuthenticator>
-      </>
-    );
+    appContent = <Auth>{appContent}</Auth>;
   }
 
   return (
@@ -110,10 +59,11 @@ export default function Root({ store, persistor }: RootType) {
         onBeforeLift={() => {
           // @ts-ignore
           // eslint-disable-next-line react/prop-types
-          if(!AppConfig.isWeb ) { // || store.app.user !== undefined
+          if (!AppConfig.isWeb) {
+            // || store.app.user !== undefined
             // @ts-ignore
             // eslint-disable-next-line react/prop-types
-            store.dispatch(AppActions.initApp())
+            store.dispatch(AppActions.initApp());
           }
         }}
         // @ts-ignore
