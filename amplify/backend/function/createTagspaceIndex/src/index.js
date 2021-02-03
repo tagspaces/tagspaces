15426e0e972b4a79be0ab1a18965c756b73f58fb
stanimir <s.marinov@structmedia.com>
2021-02-03 10:50:05 +0200

    update thumbnails generation function version

--- amplify/backend/function/createTagspaceIndex/src/index.js
+++ amplify/backend/function/createTagspaceIndex/src/index.js
@@ -10,11 +10,17 @@ exports.handler = async (event, context, callback) => {
   console.log('Received event:', JSON.stringify(event, null, 2));
   const bucketName = event.Records[0].s3.bucket.name;
   const { key } = event.Records[0].s3.object;
+  const { eventName } = event.Records[0];
   /* const path = '/';
   const directoryIndex = await indexer(path, bucketName);
   const success = await persistIndex(path, directoryIndex, bucketName); */
-
-  thumbnailsHandler(bucketName, key, callback);
+  try {
+    await thumbnailsHandler(eventName, bucketName, key);
+    callback(null, event);
+  } catch (e) {
+    console.error(e);
+    callback(e);
+  }
   /* if (success) {
     // console.log('createIndex: ' + JSON.stringify(directoryIndex));
     context.succeed({});
