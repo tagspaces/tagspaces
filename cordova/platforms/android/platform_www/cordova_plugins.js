1e2d7f9fad9299b0d9cb0c823ac55ad7d332211b
Stanimir <sytolk@gmail.com>
2021-03-19 12:35:06 +0200

    Android app should show all kind of files not only images (#1365)

--- cordova/platforms/android/platform_www/cordova_plugins.js
+++ cordova/platforms/android/platform_www/cordova_plugins.js
@@ -1,6 +1,14 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
   module.exports = [
     {
+      "id": "com-darryncampbell-cordova-plugin-intent.IntentShim",
+      "file": "plugins/com-darryncampbell-cordova-plugin-intent/www/IntentShim.js",
+      "pluginId": "com-darryncampbell-cordova-plugin-intent",
+      "clobbers": [
+        "intentShim"
+      ]
+    },
+    {
       "id": "com.ourcodeworld.plugins.Filebrowser.Filebrowser",
       "file": "plugins/com.ourcodeworld.plugins.Filebrowser/www/filebrowser.js",
       "pluginId": "com.ourcodeworld.plugins.Filebrowser",
@@ -267,17 +275,10 @@ cordova.define('cordova/plugin_list', function(require, exports, module) {
       "clobbers": [
         "window.plugins.socialsharing"
       ]
-    },
-    {
-      "id": "com-darryncampbell-cordova-plugin-intent.IntentShim",
-      "file": "plugins/com-darryncampbell-cordova-plugin-intent/www/IntentShim.js",
-      "pluginId": "com-darryncampbell-cordova-plugin-intent",
-      "clobbers": [
-        "intentShim"
-      ]
     }
   ];
   module.exports.metadata = {
+    "com-darryncampbell-cordova-plugin-intent": "2.0.0",
     "com.ourcodeworld.plugins.Filebrowser": "1.0.0",
     "com.whebcraft.cordova.plugin.downloader": "0.1.0",
     "cordova-plugin-add-swift-support": "2.0.2",
@@ -290,7 +291,6 @@ cordova.define('cordova/plugin_list', function(require, exports, module) {
     "cordova-plugin-file-opener2": "3.0.4",
     "cordova-plugin-whitelist": "1.3.4",
     "es6-promise-plugin": "4.2.2",
-    "cordova-plugin-x-socialsharing": "5.6.8",
-    "com-darryncampbell-cordova-plugin-intent": "2.0.0"
+    "cordova-plugin-x-socialsharing": "5.6.8"
   };
 });
