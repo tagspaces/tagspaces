ea1b6475c6b0fc3d77ad36c1fe6c3e2dd1c2a288
Stanimir <stanimir.marinov@structmedia.com>
2021-04-23 13:50:11 +0300

    fix tests

--- tests/e2e/general.helpers.js
+++ tests/e2e/general.helpers.js
@@ -100,7 +100,7 @@ export async function addInputKeys(tid, value) {
   const element = await global.client.$('[data-tid=' + tid + ']');
   await element.waitUntil(
     async function() {
-      const displayed = await this.isDisplayedInViewport();
+      const displayed = await this.isDisplayed(); // isDisplayedInViewport();
       return displayed === true;
     },
     {
@@ -115,7 +115,7 @@ export async function addInputKeys(tid, value) {
   await elemInput.waitUntil(
     async function() {
       // const displayed = await this.isDisplayed();
-      const displayed = await this.isDisplayedInViewport();
+      const displayed = await this.isDisplayed(); // isDisplayedInViewport();
       return displayed === true;
     },
     {
