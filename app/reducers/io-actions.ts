cc8dfddaeea0f1a288d466760d8e38ccce286279
Stanimir <sytolk@gmail.com>
2021-06-25 16:23:52 +0300

    fix cordova android upload files (#1459)

--- app/reducers/io-actions.ts
+++ app/reducers/io-actions.ts
@@ -210,11 +210,10 @@ const actions = {
         reader.onload = async (event: any) => {
           await readerLoaded(event, inx, filePath);
         };
-        if (AppConfig.isCordova) {
+        /* if (AppConfig.isCordova) {
           reader.readAsDataURL(file);
-        } else {
-          reader.readAsArrayBuffer(file);
-        }
+        } else { */
+        reader.readAsArrayBuffer(file);
       }
 
       async function readerLoaded(event, index, fileTargetPath) {
