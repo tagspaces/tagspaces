9ab90c074456ce0c94455f09f94a4b28cc92fe4e
Ilian Sapundshiev <ilian@tagspaces.org>
2021-07-15 17:17:15 +0200

    fix: removing unneeded file

--- app/services/menuFactory.ts
+++
@@ -1,9 +0,0 @@
-export function buildMenu(i18n) {
-  /* if (config.platform === 'darwin') {
-    this.menu = Menu.buildFromTemplate(darwinTemplate(app, mainWindow, i18n));
-    Menu.setApplicationMenu(this.menu);
-  } else {
-    this.menu = Menu.buildFromTemplate(otherTemplate(app, mainWindow, i18n));
-    mainWindow.setMenu(this.menu)
-  } */
-}
