73f11b958528efae47b4f389dbce749d166acb40
Stanimir <sytolk@gmail.com>
2021-08-25 21:20:36 +0300

    check if npm package is installed before to install

---
+++ scripts/postinstall.js
@@ -0,0 +1,48 @@
+const fs = require('fs-extra');
+const npm = require('npm');
+
+function isInstalled(npmPackage) {
+  try {
+    const path = require.resolve('tagspaces-common-index');
+    const data = fs.readFileSync(path, 'utf8');
+    return data.indexOf(npmPackage) !== -1; // fs.existsSync(path)
+    // process.moduleLoadList.indexOf("NativeModule " + npmPackage) >= 0 ||
+  } catch (e) {
+    return false;
+  }
+}
+if (process.env.PD_PLATFORM === 'node') {
+  if (!isInstalled('tagspaces-common-node')) {
+    npm.load(er => {
+      if (er) {
+        console.log('err:', er);
+        return;
+      }
+      npm.commands.run(['postinstall-node'], err => {
+        if (err) {
+          console.log('err:', err);
+        }
+      });
+      npm.on('log', message => {
+        console.log('npm:' + message);
+      });
+    });
+  }
+} else if (process.env.PD_PLATFORM === 'web') {
+  if (!isInstalled('tagspaces-common-aws')) {
+    npm.load(er => {
+      if (er) {
+        console.log('err:', er);
+        return;
+      }
+      npm.commands.run(['postinstall-web'], err => {
+        if (err) {
+          console.log('err:', err);
+        }
+      });
+      npm.on('log', message => {
+        console.log('npm:' + message);
+      });
+    });
+  }
+}
