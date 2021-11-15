ec4167a402153176313db618d9323bb82f4ce93a
Stanimir <sytolk@gmail.com>
2021-11-15 15:12:47 +0200

    fix platform install

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -1,10 +1,7 @@
 const fs = require('fs-extra');
 const pathLib = require('path');
-const npm = require('npm');
+const shell = require('shelljs');
 const packageJson = require('../package.json');
-
-process.env.ts_platform_version =
-  packageJson.dependencies['@tagspaces/tagspaces-platforms'];
 
 function isInstalled(npmPackage) {
   // TODO check installed version
@@ -23,8 +20,59 @@ function isInstalled(npmPackage) {
     return false;
   }
 }
-if (process.env.PD_PLATFORM === 'node') {
-  if (!isInstalled('@tagspaces/tagspaces-common-node')) {
+
+let install = false;
+if (process.env.PD_PLATFORM === 'electron') {
+  if (!isInstalled('@tagspaces/tagspaces-common-electron')) {
+    install = true;
+  }
+} else if (process.env.PD_PLATFORM === 'node') {
+  if (
+    !isInstalled('@tagspaces/tagspaces-common-node') ||
+    isInstalled('@tagspaces/tagspaces-common-electron')
+  ) {
+    install = true;
+  }
+} else if (process.env.PD_PLATFORM === 'web') {
+  if (!isInstalled('@tagspaces/tagspaces-common-aws')) {
+    install = true;
+  }
+} else if (process.env.PD_PLATFORM === 'cordova') {
+  if (!isInstalled('@tagspaces-common-cordova')) {
+    install = true;
+  }
+}
+
+if (
+  install &&
+  shell.exec(
+    'yarn upgrade @tagspaces/tagspaces-platforms@' +
+      packageJson.dependencies['@tagspaces/tagspaces-platforms']
+  ).code !== 0
+) {
+  shell.echo('Error: Install ' + process.env.PD_PLATFORM + ' platform failed');
+  shell.exit(1);
+}
+
+/* if (process.env.PD_PLATFORM === 'electron') {
+  if (!isInstalled('@tagspaces/tagspaces-common-electron')) {
+    npm.load(er => {
+      if (er) {
+        console.log('err:', er);
+        return;
+      }
+      npm.commands.run(['postinstall-electron'], err => {
+        if (err) {
+          console.log('err:', err);
+        }
+      });
+      npm.on('log', message => {
+        console.log('npm:' + message);
+      });
+    });
+  }
+} else if (process.env.PD_PLATFORM === 'node') {
+  if (!isInstalled('@tagspaces/tagspaces-common-node') || isInstalled('@tagspaces/tagspaces-common-electron')) {
     npm.load(er => {
       if (er) {
         console.log('err:', er);
@@ -74,4 +122,4 @@ if (process.env.PD_PLATFORM === 'node') {
       });
     });
   }
-}
+} */
