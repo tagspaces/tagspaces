6b3dde5729da23b6b9dc31133973d4b7f9261859
Ilian Sapundshiev <ilian@tagspaces.org>
2022-01-17 12:03:05 +0100

    fix: enable PWA to be installed in subfolders

--- web/pwa.js
+++ web/pwa.js
@@ -3,7 +3,7 @@
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
     navigator.serviceWorker
-      .register('/sw.js')
+      .register('sw.js')
       .then(reg => {
         document.title = 'TagSpaces - ' + document.location.hostname;
         // console.log('TagSpaces service worker registered.', reg);
