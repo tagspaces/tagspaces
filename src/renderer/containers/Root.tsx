64766d04434458d2cd989e2daf993fb8df3076cb
Stanimir <sytolk@gmail.com>
2022-05-23 19:03:12 +0300

    project structure

---
+++ src/renderer/containers/Root.tsx
@@ -0,0 +1,75 @@
+/**
+ * TagSpaces - universal file and folder organizer
+ * Copyright (C) 2017-present TagSpaces UG (haftungsbeschraenkt)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License (version 3) as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
+
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { Store } from 'redux';
+import LoadingScreen from '../components/LoadingScreen';
+import { actions as AppActions } from '../reducers/app';
+import AppConfig from '../config';
+import App from '../containers/App';
+import MainPage from '../containers/MainPage';
+import TsAuth from '../containers/TsAuth';
+
+type RootType = {
+  store: Store<{}>;
+  persistor: {};
+};
+
+export default function Root({ store, persistor }: RootType) {
+  let appContent = (
+    <App>
+      <MainPage />
+    </App>
+  );
+
+  if (AppConfig.isWeb) {
+    appContent = <TsAuth>{appContent}</TsAuth>;
+  }
+
+  return (
+    <Provider
+      // @ts-ignore
+      store={store}
+    >
+      {/**
+       * PersistGate delays the rendering of the app's UI until the persisted state has been retrieved
+       * and saved to redux.
+       * The `loading` prop can be `null` or any react instance to show during loading (e.g. a splash screen),
+       * for example `loading={<SplashScreen />}`.
+       * @see https://github.com/rt2zz/redux-persist/blob/master/docs/PersistGate.md
+       */}
+      <PersistGate
+        loading={<LoadingScreen />}
+        onBeforeLift={() => {
+          // eslint-disable-next-line react/prop-types
+          if (!AppConfig.isAmplify) {
+            // || store.app.user !== undefined
+            // @ts-ignore
+            // eslint-disable-next-line react/prop-types
+            store.dispatch(AppActions.initApp());
+          }
+        }}
+        // @ts-ignore
+        persistor={persistor}
+      >
+        {appContent}
+      </PersistGate>
+    </Provider>
+  );
+}
---
+++ src/renderer/index.tsx
@@ -0,0 +1,37 @@
+/**
+ * TagSpaces - universal file and folder organizer
+ * Copyright (C) 2017-present TagSpaces UG (haftungsbeschraenkt)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License (version 3) as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
+
+import { createRoot } from 'react-dom/client';
+import Root from './containers/Root';
+
+import configureStore from './store/configureStore';
+import './app.global.css';
+
+const { store, persistor } = configureStore({});
+
+const container = document.getElementById('root')!;
+const root = createRoot(container);
+root.render(<Root store={store} persistor={persistor} />);
+
+// calling IPC exposed from preload script
+/*window.electron.ipcRenderer.once('ipc-example', (arg) => {
+  // eslint-disable-next-line no-console
+  console.log(arg);
+});
+window.electron.ipcRenderer.sendMessage('ipc-example', ['ping']);*/
+
