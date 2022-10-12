0392d034c78bf3d16fd60785f36b8a352b361ba3
sytolk <s.marinov@structmedia.com>
2022-10-12 19:24:22 +0300

    e2e debug

--- tests/e2e/clearstorage.helpers.js
+++ tests/e2e/clearstorage.helpers.js
@@ -1,7 +1,8 @@
-import { clickOn } from './general.helpers';
+import { clickOn, takeScreenshot } from './general.helpers';
 
 export async function clearStorage() {
-  await clickOn('[data-tid=settings]');
+  await takeScreenshot('clearStorage');
+  await clickOn('[data-tid=settings]', { timeout: 20000 });
   await clickOn('[data-tid=advancedSettingsDialogTID]');
   await clickOn('[data-tid=resetSettingsTID]');
   await clickOn('[data-tid=confirmResetSettingsDialogTID]');
