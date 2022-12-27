b1029f22d081eeb7e4747e2c3ea12f9d4f9d42c9
Ilian Sapundshiev <ilian@tagspaces.org>
2022-12-27 22:49:07 +0100

    allow saving files(screenshot) from viewer/editor extensions

--- app/components/FileView.tsx
+++ app/components/FileView.tsx
@@ -131,7 +131,7 @@ function FileView(props: Props) {
           }}
           src={fileOpenerURL}
           allowFullScreen
-          sandbox="allow-same-origin allow-scripts allow-modals"
+          sandbox="allow-same-origin allow-scripts allow-modals allow-downloads"
           title={i18n.t('core:fileViewer')}
           id="FileViewer"
         />
