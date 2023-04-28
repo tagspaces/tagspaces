9e24ee6b7f26c278822f24facae301a6d03f9a40
Stanimir <sytolk@gmail.com>
2023-04-28 17:14:09 +0300

    fix-moveDirectory-in-KanBan-S3-location (#1921)

--- playwright.unit.js
+++ playwright.unit.js
@@ -11,5 +11,5 @@ module.exports = defineConfig({
   timeout: 30000,
   // (3 hours) Maximum time in milliseconds the whole test suite can run. Useful on CI to prevent broken setup from running too long and wasting resources.
   globalTimeout: process.env.CI ? 3 * 60 * 60 * 1000 : undefined,
-  reporter: process.env.CI ? 'github' : 'list',
+  reporter: process.env.CI ? 'github' : 'list'
 });
