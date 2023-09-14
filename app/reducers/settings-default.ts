ffc06fbd12c33c0f8493bc4083799d073e733e22
sytolk <s.marinov@structmedia.com>
2023-09-14 16:19:53 +0300

    fix: state after reload app is always first tab opened

--- app/reducers/settings-default.ts
+++ app/reducers/settings-default.ts
@@ -53,7 +53,7 @@ export default {
   newHTMLFileContent:
     '<!DOCTYPE html><html><head><meta http-equiv="Content-Type" content="text/html; charset=utf-8"></head><body></body></html>',
   showUnixHiddenEntries: false,
-  entryContainerTab: 0,
+  entryContainerTab: undefined,
   checkForUpdates,
   reorderTags: false,
   coloredFileExtension: false,
