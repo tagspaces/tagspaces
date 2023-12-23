e6203b76b97cc0507bc02b5a32d105c9aa73dbac
Ilian Sapundshiev <ilian@tagspaces.org>
2023-12-23 20:32:42 +0100

    chore: disable dev menu on start in debug modus

--- src/main/main.ts
+++ src/main/main.ts
@@ -52,7 +52,7 @@ const isDebug =
   process.env.NODE_ENV === 'development' || process.env.DEBUG_PROD === 'true';
 
 if (isDebug) {
-  require('electron-debug')();
+  require('electron-debug')({ showDevTools: false });
 }
 
 const testMode = process.env.NODE_ENV === 'test';
