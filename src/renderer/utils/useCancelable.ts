15df3fb59cbc0674594ba6d15cc8e0cb1f8599f8
sytolk <s.marinov@structmedia.com>
2024-06-14 13:05:25 +0300

    add Copyright

--- src/renderer/utils/useCancelable.ts
+++ src/renderer/utils/useCancelable.ts
@@ -1,6 +1,6 @@
 /**
  * TagSpaces - universal file and folder organizer
- * Copyright (C) 2017-present TagSpaces UG (haftungsbeschraenkt)
+ * Copyright (C) 2024-present TagSpaces GbmH
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License (version 3) as
