631da466f300010e2ed54aa0f09dc0684750245e
Ilian Sapundshiev <ilian@tagspaces.org>
2024-09-20 10:53:43 +0200

    chore: added default extconfig.js

--- web/extconfig.tmpl.js
+++ web/extconfig.tmpl.js
@@ -2,3 +2,83 @@
 
 // Uncomment the following line to enable saving the location data in the browser's local storage
 // window.ExtSaveLocationsInBrowser = true;
+
+// Uncomment the next section to get a custom tag library
+// window.ExtTagLibrary = [
+//     {
+//       uuid: "17882854-44a7-4b2d-a2b1-b022846ac41d",
+//       title: "Common Tags",
+//       color: "#008000",
+//       textcolor: "#ffffff",
+//       children: [
+//         {
+//           type: "plain",
+//           title: "book",
+//           description: "",
+//           color: "#008000",
+//           textcolor: "#ffffff",
+//         },
+//         {
+//           type: "plain",
+//           title: "article",
+//           description: "",
+//           color: "#008000",
+//           textcolor: "#ffffff",
+//         },
+//       ],
+//     },
+//     {
+//       uuid: "e21711da-ee78-4c83-bae3-e0007fe426a3",
+//       title: "Priorities",
+//       color: "#008000",
+//       textcolor: "#ffffff",
+//       children: [
+//         {
+//           type: "plain",
+//           title: "high",
+//           description: "",
+//           color: "#ff7537",
+//           textcolor: "#ffffff",
+//         }
+//       ],
+//     },
+//   ];
+
+// Uncomment the following section to get two pre-configured locations
+// window.ExtLocations = [
+//     {
+//       "uuid": "some-uuid",
+//       "name": "CloudflareR2 Location",
+//       "type": "1",
+//       "path": "",
+//       "creationDate": 1720772062210,
+//       "lastEditedDate": 1726821791168,
+//       "isDefault": false,
+//       "isReadOnly": false,
+//       "disableIndexing": false,
+//       "disableThumbnailGeneration": false,
+//       "fullTextIndex": false,
+//       "accessKeyId": "your access key",
+//       "secretAccessKey": "your secret key",
+//       "bucketName": "your bucket name",
+//       "region": "",
+//       "endpointURL": "https://your-cloudflare-instance.eu.r2.cloudflarestorage.com"
+//     },
+//     {
+//       "uuid": "some-other-uuid",
+//       "name": "S3Proxy Location",
+//       "type": "1",
+//       "path": "",
+//       "creationDate": "2023-11-14T11:27:57.356Z",
+//       "lastEditedDate": 1726821791168,
+//       "isDefault": false,
+//       "isReadOnly": false,
+//       "disableIndexing": false,
+//       "fullTextIndex": false,
+//       "accessKeyId": "your access key",
+//       "secretAccessKey": "your secret key",
+//       "bucketName": "your bucket name",
+//       "region": "",
+//       "endpointURL": "http://192.168.0.78:8000"
+//     }
+//   ]
