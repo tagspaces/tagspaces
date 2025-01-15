a36ebdf328d2ca6c3ad82d1b0e3942a5d5515ab3
sytolk <s.marinov@structmedia.com>
2025-01-15 10:20:08 +0200

    add prebuild third party extensions in web

--- scripts/generateExtensionsConfig.ts
+++ scripts/generateExtensionsConfig.ts
@@ -1,6 +1,8 @@
 import fs from 'fs';
 import path from 'path';
 import { getExtensions } from '../src/main/extension-utils';
+//import { unZip } from '@tagspaces/tagspaces-common-node/io-node';
+//import { Extensions } from "../src/main/types";
 // const os = require('os');
 
 let directoryPath = path.join(
@@ -11,9 +13,14 @@ let directoryPath = path.join(
   'node_modules',
 );
 
+const args = process.argv.slice(2);
+/*extractZips(args).then(() => {
+
+})*/
 getExtensions(directoryPath, [
   '@tagspaces/extensions',
   '@tagspacespro/extensions',
+  ...(args.length > 0 ? args : []),
 ])
   .then(({ extensions, supportedFileTypes }) => {
     writeExtensions(extensions, supportedFileTypes);
@@ -56,6 +63,67 @@ function writeExtensions(extensions, supportedFileTypes) {
     console.log('Successfully generated:' + outputFile);
   });
 }
+
+/*async function extractZips(dirs: string[]): Promise<boolean> {
+  return new Promise((resolve, reject) => {
+    const promises = dirs.map(dir => extractZip(dir, path.join(dir, 'extensions')));
+    Promise.allSettled(promises).then((results) => {
+      const fulfilledResults = results
+        .filter((result) => result.status === 'fulfilled')
+        .map((result) => (result as PromiseFulfilledResult<Extensions>).value);
+      console.log(fulfilledResults);
+      resolve(true);
+    });
+  });
+}*/
+/**
+ * Extract all ZIP files in a given directory.
+ * @param directoryPath - Path to the directory containing ZIP files.
+ * @param outputDirectory - Path to the directory where files will be extracted.
+ * @returns Promise<void> - Resolves when all files are extracted.
+ */
+/*const extractZip = (
+  directoryPath: string,
+  outputDirectory: string
+): Promise<boolean> => {
+  return new Promise((resolve, reject) => {
+    // Ensure the output directory exists
+    if (!fs.existsSync(outputDirectory)) {
+      fs.mkdirSync(outputDirectory, { recursive: true });
+    }
+
+    // Read all files in the directory
+    fs.readdir(directoryPath, { withFileTypes: true }, (err, files) => {
+      if (err) {
+        console.log('Error reading directory:', err);
+        //reject(err);
+        resolve(false);
+        return;
+      }
+
+      // Filter for ZIP files only
+      const zipFiles = files.filter(file => path.extname(file.name).toLowerCase() === '.zip');
+      if (zipFiles.length === 0) {
+        resolve(true);
+        return;
+      }
+
+      // Process each ZIP file
+      const promises = zipFiles.map(file => {
+        const zipPath = path.join(directoryPath, file.parentPath);
+        const extractPath = path.join(outputDirectory, path.basename(file.parentPath, '.zip'));
+        return unZip(zipPath, extractPath);
+      });
+      Promise.allSettled(promises).then((results) => {
+        const fulfilledResults = results
+          .filter((result) => result.status === 'fulfilled')
+          .map((result) => (result as PromiseFulfilledResult<Extensions>).value);
+        console.log(fulfilledResults);
+        resolve(true);
+      });
+    });
+  });
+};*/
 
 /*function getUserDataPath() {
   switch (process.platform) {
